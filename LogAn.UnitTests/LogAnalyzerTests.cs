67ef098045ca56d281dec028c6305e25eff3d391
Samuel Hong <xkrn4lyfx@yahoo.com>
2015-07-26 16:20:10 -0700

    Completed Chapter 2.

---
+++ LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidLogFileName_BadExtension_ReturnsFalse()
+        {
+            // Arrange
+            LogAnalyzer logAnalyzer = MakeLogAnalyzer();
+
+            // Act
+            bool result = logAnalyzer.IsValidLogFileName("SomeFileWithBadExtension.foo");
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void IsValidLogFileName_GoodExtensionUpperCase_ReturnsTrue()
+        {
+            // Arrange
+            LogAnalyzer logAnalyzer = MakeLogAnalyzer();
+
+            // Act
+            bool result = logAnalyzer.IsValidLogFileName("FileWithValidExtension.SLF");
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void IsValidLogFileName_GoodExtensionLowerCase_ReturnsTrue()
+        {
+            // Arrange
+            LogAnalyzer logAnalyzer = MakeLogAnalyzer();
+
+            // Act
+            bool result = logAnalyzer.IsValidLogFileName("FileWithValidExtension.slf");
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void IsValidFileName_EmptyFilename_Throws()
+        {
+            LogAnalyzer logAnalyzer = MakeLogAnalyzer();
+
+            var exception = Assert.Catch<Exception>(() => logAnalyzer.IsValidLogFileName(""));
+
+            StringAssert.Contains("Filename must be provided.", exception.Message);
+        }
+
+        [TestCase("badFile.foo", false)]
+        [TestCase("goodFile.slf", true)]
+        public void IsValidFileName_WhenCalled_ChangesWasLastFileNameValid(string fileName, bool expected)
+        {
+            LogAnalyzer logAnalyzer = MakeLogAnalyzer();
+
+            logAnalyzer.IsValidLogFileName(fileName);
+
+            Assert.AreEqual(expected, logAnalyzer.WasLastFileNameValid);
+        }
+
+        private LogAnalyzer MakeLogAnalyzer()
+        {
+            return new LogAnalyzer();
+        }
+    }
+}
---
+++ LogAn/MainConsole.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogAn;
+
+namespace ArtOfUnitTesting
+{
+    class MainConsole
+    {
+        static void Main(string[] args)
+        {
+            LogAnalyzer logAnalyzer = new LogAnalyzer();
+            if (logAnalyzer.IsValidLogFileName("hello"))
+            {
+                System.Console.WriteLine("Valid.");
+            }
+            else
+            {
+                System.Console.WriteLine("Invalid.");
+            }
+            System.Console.ReadKey();
+        }
+    }
+}
